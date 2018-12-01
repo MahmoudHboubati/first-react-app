134d7b324f8e0356f2da7564ac5b1ee6d16c57c2
Mahmoud Hboubati <mahmoud.hboubati@emaratech.ae>
2018-12-02 01:41:42 +0400

    feat(*): restructure app, add navbar and centralize data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 2 },
+      { id: 2, value: 5 },
+      { id: 3, value: 2 },
+      { id: 4, value: 8 }
+    ]
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <React.Fragment>
+      <Navbar totalCounters={this.state.counters.filter(c=>c.value>0).length}/>
+      <main className="container">
+        <Counters counters={this.state.counters} onReset={this.handleReset} onIncremenet={this.handleIncrement} onDelete={this.handleDelete} />
+      </main>
+    </React.Fragment>
     );
   }
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(counter => {
+      return counter.id != counterId;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
 }
+
 
 export default App;
